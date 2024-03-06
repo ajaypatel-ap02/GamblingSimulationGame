fd61d257a8dcd7f24ae179b29239e77ec76f466c
ajaypatel0 <krishnapc700@gmail.com>
2024-03-06 15:26:29 +0530

    [ADDED] : Ajay - Program started with a welcome Message

---
+++ src/gamblingSimulationGame/GamblingSimulationGame.java
@@ -0,0 +1,10 @@
+package gamblingSimulationGame;
+
+public class GamblingSimulationGame {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to program of Gambling Simulation of Ajay Patel");
+		
+	}
+
+}
