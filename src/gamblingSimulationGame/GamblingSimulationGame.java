c6cb8ddff74d554677f6ce756c0577b5f8a921a6
ajaypatel0 <krishnapc700@gmail.com>
2024-03-06 15:47:00 +0530

    [MODIFIED]: Ajay - game status of gambling simulation

--- src/gamblingSimulationGame/GamblingSimulationGame.java
+++ src/gamblingSimulationGame/GamblingSimulationGame.java
@@ -1,10 +1,19 @@
 package gamblingSimulationGame;
 
 public class GamblingSimulationGame {
-
+	private static final int EVERYDAY_STAKE = 100;
+	private static final int EVERY_GAME_BET = 1;
+	
+	public static void gameStatus() {
+		
+		System.out.println("Stake available for the day : "+EVERYDAY_STAKE);
+		System.out.println("Bet for every game : "+EVERY_GAME_BET);
+	
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to program of Gambling Simulation of Ajay Patel");
 		
+		gameStatus();
 	}
 
 }
