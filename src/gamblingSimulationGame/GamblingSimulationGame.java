e9a947cf48136dae0a88b279cee80614354ef1a0
ajaypatel0 <krishnapc700@gmail.com>
2024-03-07 15:35:21 +0530

    [MODIFIED] : Ajay - game played for 20 days and total winings amount calculated

--- src/gamblingSimulationGame/GamblingSimulationGame.java
+++ src/gamblingSimulationGame/GamblingSimulationGame.java
@@ -2,22 +2,23 @@ package gamblingSimulationGame;
 
 import java.util.Random;
 
+
 public class GamblingSimulationGame {
 	private static final int EVERYDAY_STAKE = 100;
 	private static final int EVERY_GAME_BET = 1;
 	private static final int WIN = 1;
 	private static final int LOOSE = 0;
+	private static final int DAYS_PLAYING = 20;
 
-	static int winStack = 0;
-	static int looseStack = 0;
+
 	
 	
-	public static void gameStatus() {
+	public static void gameStatus(int winStake,int looseStake) {
 		
 		System.out.println("Stake available for the day : "+EVERYDAY_STAKE);
 		System.out.println("Bet for every game : "+EVERY_GAME_BET);
-		System.out.println("Winning stack : "+winStack);
-		System.out.println("Loossing stack : "+looseStack);
+		System.out.println("Winning stack : "+winStake);
+		System.out.println("Loossing stack : "+looseStake);
 		System.out.println("----------------------------------------------------------");
 	}
 	
@@ -25,30 +26,36 @@ public class GamblingSimulationGame {
 
 		Random random = new Random();
 		
-		gameStatus();
 		
-		while(true) {
-		
-			int playStatus = random.nextInt(9) % 2;
+//		gameStatus(winStake,looseStake);
+		for(int i = 0; i < DAYS_PLAYING; i++) {
+			int winStake = 0;
+			int looseStake = 0;
+			while(true) {
 			
-			if(playStatus == LOOSE) {
-				looseStack++;
-			}else{
-				winStack++;			
-			}
+				int playStatus = random.nextInt(9) % 2;
 				
-			gameStatus();
-			
-			if(looseStack == EVERYDAY_STAKE / 2) {
-				break;
+				if(playStatus == LOOSE) {
+					looseStake++;
+				}else{
+					winStake++;			
+				}			
+				
+				if(looseStake == EVERYDAY_STAKE / 2) {
+					break;
+				}
+				if (winStake == EVERYDAY_STAKE / 2) {
+					break;
+				}
 			}
-			if (winStack == EVERYDAY_STAKE / 2) {
-				break;
-			}
-		}
+			System.out.println(i);
+			gameStatus(winStake,looseStake);
 			
+		}			
 	}
+	
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to program of Gambling Simulation of Ajay Patel");
 
 		gamePlay();
