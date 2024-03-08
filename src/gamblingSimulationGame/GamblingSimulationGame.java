e8930563d9655bda04aec32b63d89bb8d7f45a15
ajaypatel0 <krishnapc700@gmail.com>
2024-03-08 15:49:53 +0530

    [MODIFIED] : Ajay - UC-4 Comleted for playing game for 20 days and calculate winning and loosing amount at the end of the day

--- src/gamblingSimulationGame/GamblingSimulationGame.java
+++ src/gamblingSimulationGame/GamblingSimulationGame.java
@@ -4,54 +4,61 @@ import java.util.Random;
 
 
 public class GamblingSimulationGame {
-	private static final int EVERYDAY_STAKE = 100;
+	private static final int DAILYSTAKE = 100;
 	private static final int EVERY_GAME_BET = 1;
 	private static final int WIN = 1;
 	private static final int LOOSE = 0;
 	private static final int DAYS_PLAYING = 20;
 
-
-	
-	
-	public static void gameStatus(int winStake,int looseStake) {
+	private static int[] winningAmount = new int[20]; 
+	private static int[] loosingAmount = new int[20];
+	public static void gameStatus() {
 		
-		System.out.println("Stake available for the day : "+EVERYDAY_STAKE);
+		System.out.println("Stake available for start of the day : "+DAILYSTAKE);
 		System.out.println("Bet for every game : "+EVERY_GAME_BET);
-		System.out.println("Winning stack : "+winStake);
-		System.out.println("Loossing stack : "+looseStake);
-		System.out.println("----------------------------------------------------------");
+		System.out.println("--------------------------\n");
+		
 	}
 	
 	public static void gamePlay() {
 
 		Random random = new Random();
-		
-		
-//		gameStatus(winStake,looseStake);
+
 		for(int i = 0; i < DAYS_PLAYING; i++) {
-			int winStake = 0;
+
+			gameStatus();
+			
+			int winStake = 0 ;
 			int looseStake = 0;
+			
+			System.out.println("Day : "+(i+1));
+			
 			while(true) {
 			
 				int playStatus = random.nextInt(9) % 2;
 				
-				if(playStatus == LOOSE) {
+				if(playStatus != WIN) {
 					looseStake++;
 				}else{
 					winStake++;			
-				}			
-				
-				if(looseStake == EVERYDAY_STAKE / 2) {
-					break;
 				}
-				if (winStake == EVERYDAY_STAKE / 2) {
+				
+				loosingAmount[i] += looseStake;
+				winningAmount[i] += winStake;
+				
+				
+				if(looseStake == DAILYSTAKE / 2 || winStake == DAILYSTAKE / 2) {
 					break;
 				}
 			}
-			System.out.println(i);
-			gameStatus(winStake,looseStake);
 			
-		}			
+			System.out.println("Winnings for day "+(i+1)+" : "+winningAmount[i]);
+			System.out.println("loosings for day "+(i+1)+" : "+loosingAmount[i]);
+		}	
+		
+	}
+	public static void total_Winning() {
+		
 	}
 	
 	public static void main(String[] args) {
